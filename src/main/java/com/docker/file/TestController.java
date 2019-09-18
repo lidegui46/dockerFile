8f11dde578113c6907d673f54d1f6e0f6759b24d
ldg <lidegui@ccuol.com>
2019-09-18 20:12:20 +0800

    优化10

--- src/main/java/com/docker/file/TestController.java
+++ src/main/java/com/docker/file/TestController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author:  ldg
  * Date:    2019/9/17 22:13
@@ -15,6 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("get")
     public String get(@RequestParam(value = "arg", defaultValue = "") String arg) {
-        return "hello " + arg;
+        List<User> list = new ArrayList<>();
+        while (true) {
+            list.add(new User("aa_", 1));
+        }
+        //return "hello " + arg;
+    }
+}
+
+class User {
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 }
